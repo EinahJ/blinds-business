473ba1a76d30d2e2879319383f42f7541fcad4ca
ACIE <114001078+KuyaAC@users.noreply.github.com>
2023-07-29 05:46:57 +0800

    ibalik ang popup

--- home.php
+++ home.php
@@ -434,6 +434,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         margin-bottom: 5px;
     }
 
+    .popup {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
+}
+
 .popup-content {
     position: absolute;
     top: 50%;
